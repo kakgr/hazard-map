ad9f3a490a96ab352fdca509d0c8855486afd0fa
kaki <kazutan.sato@gmail.com>
2026-03-06 10:56:29 +0900

    move: 正しいパスへ移動

--- hazard_map/hazard_mapApp.swift
+++ hazard_map/hazard_mapApp.swift
@@ -1,13 +1,6 @@
-//
-//  hazard_mapApp.swift
-//  hazard map
-//
-//  Created by カカキ・カカキ on 2026/03/05.
-//
-
 import SwiftUI
-
 @main
+
 struct hazard_mapApp: App {
     var body: some Scene {
         WindowGroup {
